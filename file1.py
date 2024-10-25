78314f60dfce8d4a18a7be2429469748c82a50cf
Murali <murali15062003@gmail.com>
2024-10-25 17:04:30 +0530

    This is the second commit

--- file1.py
+++ file1.py
@@ -3,3 +3,4 @@ Riya ekkada is the trending templete in the internet no a day.
 #Many people think that there are many offer but having no required kill for getting that job..!
 Make more than required is not for the people
 and there are may more than required for the people.
+fmngjkgjggjghjghgjighi
