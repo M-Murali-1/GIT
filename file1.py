28cbaea2c713337d2880428e76a5d47a4e2477fe
Murali <murali15062003@gmail.com>
2024-10-25 17:00:02 +0530

    First Commit for the File1.py

---
+++ file1.py
@@ -0,0 +1,5 @@
+#Thia ia a good job to be done by me
+Riya ekkada is the trending templete in the internet no a day.
+#Many people think that there are many offer but having no required kill for getting that job..!
+Make more than required is not for the people
+and there are may more than required for the people.
